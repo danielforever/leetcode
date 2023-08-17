8ce45b97ca6292ce5468fead8e1df6ce069f9ede
danielforever <daniel87011354@gmail.com>
2023-08-16 22:38:58 -0400

    question 4

---
+++ 8_16_2023/159 Longest Substring with At Most Two Distinct Characters.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/description/
+
+# 22:10
+# 22:37
+# 00:27
+class Solution:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        leftPointer = 0
+        rightPointer = 0
+        count = 0
+        wordDict = {}
+        maxLength = 0
+        while(rightPointer < len(s)):
+            if(s[rightPointer] in wordDict):
+                if(wordDict.get(s[rightPointer]) == 0): count += 1
+                wordDict[s[rightPointer]] += 1 
+            else:
+                wordDict[s[rightPointer]] = 1
+                count += 1 
+            while(count > 2):
+                wordDict[s[leftPointer]] -= 1
+                if(wordDict.get(s[leftPointer]) == 0):
+                    count -= 1
+                leftPointer += 1 
+            maxLength = max(maxLength, rightPointer - leftPointer + 1)
+            rightPointer += 1    
+        return maxLength    
+                
