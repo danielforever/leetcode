a03a45efa4c0effc2b6c048ee1c19182f0d39089
danielforever <daniel87011354@gmail.com>
2023-08-16 20:30:50 -0400

    8_16_first

---
+++ 8_16_2023/73 Set Matrix Zeroes.py
@@ -0,0 +1,23 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if(matrix[i][j] == 0):
+                    x = 0
+                    y = 0
+                    while(x < len(matrix)):
+                        if(x != i and matrix[x][j] != 0):
+                            matrix[x][j] = float('-inf') 
+                        x += 1
+                    while(y < len(matrix[0])):
+                        if(y != j and matrix[i][y] != 0):
+                            matrix[i][y] = float('-inf') 
+                        y += 1
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == float('-inf'):
+                    matrix[i][j] = 0
